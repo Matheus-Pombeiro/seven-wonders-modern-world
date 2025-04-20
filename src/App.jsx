005830e8793d260540bb21528f171c661bcd76d1
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2025-04-20 16:20:58 -0300

    feat: main component imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import Main from './components/Main'
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <Header />
+      <Main />
     </>
   )
 }
