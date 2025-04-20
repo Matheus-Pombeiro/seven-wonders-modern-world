a7157d78a6fb754428dddd7ff4f57cac3241e32c
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2025-04-19 21:09:09 -0300

    feat: header

---
+++ src/components/Header.jsx
@@ -0,0 +1,51 @@
+import ImageButton from "./ImageButton"
+
+const Header = () => {
+
+    return (
+        <header
+            className="w-full h-16 px-3 bg-white flex flex-row justify-between items-center 
+            border-b border-b-orange-400 shadow-sm"
+        >
+            {/* Logo */}
+            <figure
+                className="flex flew-row justify-between items-center gap-1"
+            >
+                <img 
+                    className="size-16 xl:size-14"
+                    src="../src/assets/icons/compass-logo.png" 
+                    alt="Logo" 
+                />
+                <figcaption
+                    className="text-xl font-medium tracking-wider"
+                >
+                    7WMW
+                </figcaption>
+            </figure>
+
+            {/* Buttons */}
+            <div
+                className="flex flew-row justify-between items-center gap-2"
+            >
+                {/* Flags */}
+                <ImageButton 
+                    src="./src/assets/icons/brazil-flag.png" 
+                    alt="Brazil flag" 
+                />
+                <ImageButton 
+                    src="./src/assets/icons/usa-flag.png" 
+                    alt="USA flag" 
+                />
+
+                {/* Theme */}
+                <ImageButton 
+                    src="./src/assets/icons/sun.png" 
+                    alt="Theme" 
+                />
+            </div>
+        </header>
+    )
+
+}
+
+export default Header;
