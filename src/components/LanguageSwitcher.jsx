d5034d743d37c25f93ae58a7c882846411d8e24a
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2025-04-22 14:31:45 -0300

    feat: usage of hooks and localStorage to save the user preferences

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -2,21 +2,47 @@ import { useTranslation } from "react-i18next";
 
 import { LiaFlagUsaSolid } from "react-icons/lia";
 import { GiBrazilFlag } from "react-icons/gi";
+import { useEffect } from "react";
 
-const LanguageSwitcher = ({ btnSet }) => {
+const LanguageSwitcher = ({ isDarkMode }) => {
     const { i18n } = useTranslation(); // Translation reference
+
+    // Checking if there is a language saved in the localStorage and updating the current language
+    useEffect(() => {
+        const storedLanguage = localStorage.getItem("language");
+        console.log("Língua armazenada:", storedLanguage);
+        if (storedLanguage) {
+            setTimeout(() => {
+                i18n.changeLanguage(storedLanguage);
+            }, 10)
+        }
+    }, [i18n])
+
+    // Saving the language preference
+    const handleLanguageChange = (languageCode) => {
+        i18n.changeLanguage(languageCode);
+        localStorage.setItem("language", languageCode);
+    };
+
+    // Setting the default icon color
+    const defaultIconColor = isDarkMode ? "white" : "black";
+
+    // Button settings
+    const btnSet = {
+        size: 40,
+    };
 
     // Declaring an array to keep some of the language data
     const languageOptions = [
         {
             value: "en",
-            flag: <LiaFlagUsaSolid {...btnSet} />
+            flag: <LiaFlagUsaSolid {...btnSet} color={defaultIconColor} />
         },
         {
             value: "pt",
-            flag: <GiBrazilFlag {...btnSet} />
+            flag: <GiBrazilFlag {...btnSet} color={defaultIconColor} />
         }
-    ]
+    ];
 
     return (
         <>
@@ -24,9 +50,7 @@ const LanguageSwitcher = ({ btnSet }) => {
                 <button
                     key={index}
                     className="cursor-pointer"
-                    onClick={() => {
-                        i18n.changeLanguage(language.value)
-                    }}
+                    onClick={() => handleLanguageChange(language.value)}
                 >
                     {language.flag}
                 </button>
