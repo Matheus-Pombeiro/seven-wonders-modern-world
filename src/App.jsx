f305cf3242626ab04a3a92b6f86296955b7f9edc
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2025-05-16 00:06:29 -0300

    feat: setted routes with React Router Dom library

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
 import './App.css'
-import Header from './components/Header'
-import Main from './components/Main'
-import Footer from './components/Footer'
+
 import './translation/i18nify.js'
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomePage from './components/HomePage.jsx'
+import WonderDetails from './components/WonderDetails.jsx'
 
 const App = () => {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Router>
       <Header />
-      <Main />
+      <Routes>
+        {/* Defining the home page route */}
+        <Route path='/' element={<HomePage />} />
+
+        {/* Defining a route for each wonder using a dynamic segment */}
+        <Route path='/wonder/:name' element={<WonderDetails />} />
+      </Routes>
       <Footer />
-    </>
+    </Router>
   )
 }
 
